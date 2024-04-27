f9d2f31ececf144b5d798c83381d06d36b73344d
quannhg <quan.nguyenhg12@gmail.com>
2024-04-27 21:04:01 +0700

    fix: useChangeDirectory

--- src/handlers/useChangeDirectory.ts
+++ src/handlers/useChangeDirectory.ts
@@ -1,19 +1,24 @@
-import { PATH_IS_REQUIRED } from '@constants';
 import { usePwdStore } from '@states';
-import { inferPath, normalizePath } from '@utils';
+import { inferPath, normalizePath, extractArguments } from '@utils';
 
-export const useChangeDirectory = (): ((directory: string) => Promise<void>) => {
+export const useChangeDirectory = (): ((argumentsString: string) => Promise<void>) => {
   const { updatePwd, currentDirectory } = usePwdStore();
 
-  return async (directory: string) => {
-    if (!directory) throw Error(PATH_IS_REQUIRED);
+  return async (argumentsString: string) => {
+    const args = extractArguments(argumentsString);
 
-    const updatedDirectory = normalizePath(inferPath(currentDirectory, directory));
-
-    try {
-      await updatePwd(updatedDirectory);
-    } catch (err) {
-      throw err;
+    if (!args?.length) {
+      throw Error('Invalid arguments\nUsage: cd FOLDER_PATH');
     }
+
+    const folderPath = normalizePath(args.shift()!);
+
+    if (args.length > 0) {
+      throw Error('Invalid arguments\nUsage: cd FOLDER_PATH');
+    }
+
+    const updatedDirectory = inferPath(currentDirectory, folderPath);
+
+    await updatePwd(updatedDirectory);
   };
 };
