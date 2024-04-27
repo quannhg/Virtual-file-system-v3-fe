f0dce0071e051e2170ed691a2ec4d32713113e03
quannhg <quan.nguyenhg12@gmail.com>
2024-04-27 21:03:35 +0700

    fix: support --, ., .. in normalizePath

--- src/utils/normalizePath.ts
+++ src/utils/normalizePath.ts
@@ -1,13 +1,14 @@
 export function normalizePath(path: string) {
+  console.log(path);
   if (path === '') return '';
   const subpaths = path.split('/');
   return subpaths.map(normalizeSubpath).join('/');
 }
 
 function normalizeSubpath(subpath: string): string {
-  const [, group0, group1, group2] =
-    subpath.match(/^"([a-zA-Z0-9 _-]+)"|'([a-zA-Z0-9 _-]+)'|([a-zA-Z0-9 _-]+)$/) || [];
-  const res = group0 || group1 || group2;
+  const [, group0, group1, group2, group3] =
+    subpath.match(/^"([a-zA-Z0-9 _-]+)"|'([a-zA-Z0-9 _-]+)'|([a-zA-Z0-9 _-]+)|(--|\.|\.\.)$/) || [];
+  const res = group0 || group1 || group2 || group3;
   if (res === undefined) {
     throw Error('Invalid path');
   }
